d2dea006c88549ab890dd10ab67220e41f88466e
Andrew <35861377+orebitt@users.noreply.github.com>
2023-02-24 04:45:46 -0800

    week 5 Th starter code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var latitude
-var longitude
+let latitude
+let longitude
 const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 window.onload=function(){
@@ -10,49 +10,25 @@ window.onload=function(){
 
 
 	if ("geolocation" in navigator) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-		latitude = position.coords.latitude;
-		longitude = position.coords.longitude;
-		console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-	  });
-	  
+		navigator.geolocation.getCurrentPosition(success)
+
 	} else {
 	  console.log("Geolocation is not available in your browser.");
 	}
 
 	const btn = document.getElementById("getWeatherBtn")
 	btn.addEventListener("click", () => {
-	  const lat = latitude;
-	  const lon = longitude;
-	  const xhr = new XMLHttpRequest();
-	  xhr.open("GET", `http://localhost:3000/weather/${lat}/${lon}`);
-	  xhr.send();
-
-	  xhr.onload = () => {
-        const response = JSON.parse(xhr.responseText);
-		console.log(response)
-        const temperature = response.temperature;
-        const weatherStatus = response.weatherStatus;
-
-        document.getElementById("temperature").innerHTML = `Temperature: ${temperature}°F`;
-        document.getElementById("weatherStatus").innerHTML = `Weather Status: ${weatherStatus}`;
-	  };
-	  const xhr2 = new XMLHttpRequest();
-	  xhr2.open("GET", `http://localhost:3000/weather/forecast/${lat}/${lon}`);
-	  xhr2.send();
-	  xhr2.onload = function () {
-	  if (xhr2.status === 200) {
-		  var response = JSON.parse(xhr2.responseText);
-		  var forecast = response.forecast;
-		  var forecastElements = document.getElementsByClassName("forecast");
-		  for (var i = 0; i < forecast.length; i++) {
-			forecastElements[i].innerHTML = forecast[i].dayName + ": " + forecast[i].temp + "°F";
+	  var forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]]
+	  var forecastElements = document.getElementsByClassName("forecast");
+	  for (var i = 0; i < forecast.length; i++) {
+			forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "°F";
       }
-    } else {
-      console.error(xhr2.responseText);
-    }
-  };
-
-	  
 	});
+}
+
+function success(position){
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+	console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+
 }
